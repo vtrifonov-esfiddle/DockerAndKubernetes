56f9ada18ac54f5eab187b386e86e6df93d7d742
Vladimir Trifonov <vladotrifonov@gmail.com>
2019-08-26 23:00:25 +0100

    Mongo DB - backup and restore jobs

--- fullWebStackDotNet/Kubernetes/deployment-down.ps1
+++ fullWebStackDotNet/Kubernetes/deployment-down.ps1
@@ -13,29 +13,29 @@ function Remove-ReactAspnet() {
 
 function Remove-Mongodb() {
     kubectl delete deployment mongodb-deployment
-    kubectl delete service mongo
-    kubectl delete pvc mongodb-data-claim
+    kubectl delete service mongo    
     kubectl delete secret mongodb
+    kubectl delete pvc mongodb-data-claim
     kubectl delete pv mongodb-data-volume
+    kubectl delete pvc mongodb-backups-claim
+    kubectl delete pv mongodb-backups-volume
 }
 
 function Remove-MongodbWebApi() {
     kubectl delete deployment mongodb-webapi-deployment
-    kubectl delete service mongodb-webapi
     kubectl delete service mongodb-webapi-production
     kubectl delete secret mongodb-webapi
 }
 
 function Remove-SqlserverWebApi() {
     kubectl delete deployment sqlserver-webapi-deployment
-    kubectl delete service sqlserver-webapi-service
     kubectl delete service sql-server-webapi-production
     kubectl delete secret sqlserver-webapi
 }
 
 function Remove-Sqlserver() {
     kubectl delete deployment sqlserver-deployment
-    kubectl delete service sqlserver-service
+    kubectl delete service sqlserver
     kubectl delete pvc sqlserver-data-claim
     kubectl delete secret sqlserver
     kubectl delete pv sqlserver-data-volume
